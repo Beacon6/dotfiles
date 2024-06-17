4c134a1bc0af8254d1e93a12fef372cd6cd0ccba
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-06-17 22:47:33 +0200

    Set 'conceallevel' for the Neovim Obsidian plugin

--- nvim/.config/nvim/lua/beacon6/options.lua
+++ nvim/.config/nvim/lua/beacon6/options.lua
@@ -46,6 +46,8 @@ vim.opt.scrolloff = 8
 
 vim.opt.hlsearch = true
 
+vim.opt.conceallevel = 1
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
