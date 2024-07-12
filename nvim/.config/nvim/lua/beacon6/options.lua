20bcb2d790632656f179f11172454f5e81bc0c14
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-07-12 19:45:57 +0200

    Change Neovim indent options

--- nvim/.config/nvim/lua/beacon6/options.lua
+++ nvim/.config/nvim/lua/beacon6/options.lua
@@ -11,9 +11,9 @@ vim.opt.clipboard = 'unnamedplus'
 
 vim.opt.breakindent = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
