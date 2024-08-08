0a02e835e18afd6585157af7d94cf220ad191362
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2024-08-08 21:41:47 +0200

    feat: change WezTerm display options

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,4 +11,8 @@ config.enable_scroll_bar = false
 
 config.use_fancy_tab_bar = false
 
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
+config.freetype_load_target = 'Light'
+
 return config
