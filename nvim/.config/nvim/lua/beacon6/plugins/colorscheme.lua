1be822a5eb3e220cbceba64e8398be4eb0a96cdd
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2025-02-05 10:07:52 +0100

    feat: change colorscheme and remove some plugins

--- nvim/.config/nvim/lua/beacon6/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/beacon6/plugins/colorscheme.lua
@@ -1,14 +1,24 @@
+function SetColors(color)
+  color = color or "rose-pine-moon"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    init = function()
-      require("catppuccin").setup({
-        show_end_of_buffer = true,
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        disable_background = true,
+        styles = {
+          italic = false,
+        },
       })
 
-      vim.cmd.colorscheme("catppuccin-mocha")
+      SetColors()
     end,
   },
 }
