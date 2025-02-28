0bef4ee69bc59c3db9077c79a6dec2a84be84a05
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2025-02-28 19:25:24 +0100

    feat: start simplifying Neovim config

--- nvim/.config/nvim/lua/beacon6/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/beacon6/plugins/colorscheme.lua
@@ -1,24 +1,24 @@
 function SetColors(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
+    color = color or "rose-pine-moon"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				disable_background = true,
-				styles = {
-					italic = false,
-				},
-			})
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+                styles = {
+                    italic = false,
+                },
+            })
 
-			SetColors()
-		end,
-	},
+            SetColors()
+        end,
+    },
 }
