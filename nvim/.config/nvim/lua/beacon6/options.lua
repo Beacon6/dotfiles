2b0c1b65ea59d85b08bb74aaf5ea90e5399aa3b1
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2025-02-28 19:25:24 +0100

    feat: start simplifying Neovim config

--- nvim/.config/nvim/lua/beacon6/options.lua
+++ nvim/.config/nvim/lua/beacon6/options.lua
@@ -43,9 +43,9 @@ vim.opt.scrolloff = 8
 vim.opt.clipboard = "unnamedplus"
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
