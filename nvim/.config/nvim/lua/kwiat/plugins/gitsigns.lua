73a1a81d8ce0f2a35d63c53e8c4711051425ba4f
Beacon6 <61936260+Beacon6@users.noreply.github.com>
2026-08-01 12:42:03 +0200

    fix gitsigns

--- nvim/.config/nvim/lua/kwiat/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/kwiat/plugins/gitsigns.lua
@@ -4,22 +4,20 @@ return {
     config = function()
         local gitsigns = require('gitsigns')
         gitsigns.setup({
-            opts = {
-                signs = {
-                    add = { text = '▎' },
-                    change = { text = '▎' },
-                    delete = { text = '' },
-                    topdelete = { text = '' },
-                    changedelete = { text = '▎' },
-                    untracked = { text = '▎' },
-                },
-                signs_staged = {
-                    add = { text = '▎' },
-                    change = { text = '▎' },
-                    delete = { text = '' },
-                    topdelete = { text = '' },
-                    changedelete = { text = '▎' },
-                },
+            signs = {
+                add = { text = '▎' },
+                change = { text = '▎' },
+                delete = { text = '' },
+                topdelete = { text = '' },
+                changedelete = { text = '▎' },
+                untracked = { text = '▎' },
+            },
+            signs_staged = {
+                add = { text = '▎' },
+                change = { text = '▎' },
+                delete = { text = '' },
+                topdelete = { text = '' },
+                changedelete = { text = '▎' },
             },
         })
 
